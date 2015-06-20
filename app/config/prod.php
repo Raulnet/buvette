3a1afb7118184e10a3d66ba755a15f7685aa6274
raulnet <laurentnegre016@gmail.com>
2015-06-20 11:12:58 +0200

    up gitignore

--- app/config/prod.php
+++ app/config/prod.php
@@ -6,15 +6,6 @@
  * Time: 15:53
  * Doctrine (db)
  */
-$app['db.options'] = array(
-    'driver'   => 'pdo_mysql',
-    'charset'  => 'utf8',
-    'host'     => 'localhost',
-    'port'     => '3306',
-    'dbname'   => 'microcms',
-    'user'     => 'root',
-    'password' => '',
-);
 
 $app['zdb.configArray'] = array(
     'driver'   => 'pdo_mysql',
